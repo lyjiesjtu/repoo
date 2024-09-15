3d6f266bc27fb81e130fe940d7fa8587aa536c21
Edd <edgar031212@sjtu.edu.cn>
2024-09-15 19:06:18 -0400

    Update record_powertop_i7z.sh

--- record_powertop_i7z.sh
+++ record_powertop_i7z.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 输出文件路径
-OUTPUT_FILE="system_data_record.txt"
+OUTPUT_FILE="powertop_data_record.txt"
 
 # 每秒采集一次数据，直到用户手动停止
 while true
@@ -14,10 +14,6 @@ do
     echo "---- powertop data ----" >> $OUTPUT_FILE
     sudo powertop --time=1 --csv=powertop_output.csv 2>/dev/null
     cat powertop_output.csv >> $OUTPUT_FILE
-    
-    # 记录 i7z 的数据 (捕获运行时输出)
-    echo "---- i7z data ----" >> $OUTPUT_FILE
-    sudo i7z | tee -a $OUTPUT_FILE | head -n 20
 
     # 每秒采集一次
     sleep 1
